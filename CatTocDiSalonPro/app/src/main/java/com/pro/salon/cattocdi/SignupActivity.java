0279e49119fef0219f712516f3e95739ce283334
tiendat <tiendatdeveloper1997@gmail.com>
2018-10-21 21:45:59 +0700

    Tiendat Set up category when signup

--- CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/SignupActivity.java
+++ CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/SignupActivity.java
@@ -1,13 +1,19 @@
 package com.pro.salon.cattocdi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void signupStepOne(View view) {
+        Intent intent = new Intent(this, ServiceSignupActivity.class);
+        startActivity(intent);
     }
 }
