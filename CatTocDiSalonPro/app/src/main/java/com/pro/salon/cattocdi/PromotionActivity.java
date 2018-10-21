0b50f517c5d5ba87677915586438448b4e1fddd0
ngoc <ngocnt1801@gmail.com>
2018-10-21 19:02:13 +0700

    add promotion activity

--- CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/PromotionActivity.java
+++ CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/PromotionActivity.java
@@ -1,13 +1,26 @@
 package com.pro.salon.cattocdi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class PromotionActivity extends AppCompatActivity {
+
+    private TextView tvOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promotion);
+
+        tvOK = findViewById(R.id.activity_promotion_save_tv);
+        tvOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
