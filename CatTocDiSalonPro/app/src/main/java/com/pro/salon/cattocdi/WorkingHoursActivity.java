12b0927263411f2b760d2cd6227a8b0ccdc2022d
ngoc <ngocnt1801@gmail.com>
2018-10-22 14:15:35 +0700

    add contact detail

--- CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/WorkingHoursActivity.java
+++ CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/WorkingHoursActivity.java
@@ -1,13 +1,32 @@
 package com.pro.salon.cattocdi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class WorkingHoursActivity extends AppCompatActivity {
+
+    private TextView tvSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_working_hours);
+
+        tvSave = findViewById(R.id.activity_working_hours_save_tv);
+        tvSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToProfileFragment();
+            }
+        });
+
+    }
+    private void goToProfileFragment(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("fragment_id", R.id.bottom_nav_profile_item);
+        startActivity(intent);
     }
 }
