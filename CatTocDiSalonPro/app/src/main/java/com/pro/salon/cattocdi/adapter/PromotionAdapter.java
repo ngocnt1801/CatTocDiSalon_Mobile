ead8e955e4e786f540cdb4cc7130dd6ed4a959ff
Nhi Nguyen <nguyenphanthaonhi@gmail.com>
2018-11-08 15:50:34 +0700

    nhi_update_promotion

--- CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/adapter/PromotionAdapter.java
+++ CatTocDiSalonPro/app/src/main/java/com/pro/salon/cattocdi/adapter/PromotionAdapter.java
@@ -36,14 +36,14 @@ public class PromotionAdapter extends RecyclerView.Adapter<PromotionAdapter.Prom
     }
 
     @Override
-    public void onBindViewHolder(PromotionViewHolder holder, int position) {
+    public void onBindViewHolder(PromotionViewHolder holder, final int position) {
         //just for test
        /* if(position > 1){
             holder.icState.setImageResource(R.drawable.ic_stop);
         }*/
         //salon.getPromotions().get(position).getStartPeriod();
        holder.tvDate.setText(promotions.get(position).getStartToEndstr());
-       holder.tvDiscount.setText(promotions.get(position).getDiscount());
+       holder.tvDiscount.setText(String.valueOf(promotions.get(position).getDiscount()) + "%");
        holder.tvName.setText(promotions.get(position).getDescription());
        if(Calendar.getInstance().getTimeInMillis() >= promotions.get(position).getStartPeriod().getTime()){
            holder.icState.setImageResource(R.drawable.ic_stop);
@@ -56,6 +56,8 @@ public class PromotionAdapter extends RecyclerView.Adapter<PromotionAdapter.Prom
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, PromotionDetailActivity.class);
+              intent.putExtra("promotions", (Serializable) promotions.get(position));
+
                 context.startActivity(intent);
             }
         });
